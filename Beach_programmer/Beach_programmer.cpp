a2adbd5ea4cb300e0d4d692826976286c166f293
timerke <timerke@mail.ru>
2019-11-07 16:24:12 +0300

    Немного изменил код и добавил решение на C++.

---
+++ Beach_programmer/Beach_programmer.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <deque>
+#include <vector>
+
+int main()
+{
+	// Читаем входные данные
+	int n; // количество тестов
+	std::cin >> n;
+	std::deque<int>	m; // очередь из количества лежаков в каждом тесте
+	std::deque<std::vector<int>> a; // очередь из списков чисел, характеризующих лежаки в каждом тесте
+	int m_; // количество лежаков в тесте
+	int i, j, k, min_, temp; // полезные переменные, которыми будем пользоваться в дальнейшем
+	for (i = 0; i < n; i++)
+	{
+		std::cin >> m_;
+		m.push_back(m_);
+		std::vector<int> a_; // массив из чисел
+		for (j = 0; j < m_; j++)
+		{
+			std::cin >> temp;
+			a_.push_back(temp);
+		}
+		a.push_back(a_);
+	}
+
+	// Выполняем сравнения в тестах
+	for (i = 0; i < n; i++)
+	{
+		std::vector<int> a_; // массив из чисел
+		m_ = m.front(); // количество лежаков в тесте
+		a_ = a.front(); // список из чисел, характеризующих лежаки в тесте
+		min_ = a_[0] ^ a_[1]; // минимальное значение непохожести двух лежаков в тесте
+		for (j = 0; j < m_; j++)
+		{
+			for (k = j + 1; k < m_; k++)
+			{
+				if ((a_[j] ^ a_[k]) < min_)
+				{
+					min_ = a_[j] ^ a_[k];
+				}
+			}
+		}
+		std::cout << min_ << std::endl;
+		m.pop_front();
+		a.pop_front();
+	}
+}
--- Beach_programmer/Beach_programmer.py
+++ Beach_programmer/Beach_programmer.py
@@ -1,15 +1,24 @@
-n = int(input())
-min_value = []
+from collections import deque
+
+# Читаем входные данные
+n = int(input()) # количество тестов
+m = deque() # очередь из количества лежаков в каждом тесте
+a = deque() # очередь из списков чисел, характеризующих лежаки в каждом тесте
 for i in range(n):
-    m = int(input())
-    a = input().split()
-    min_ = [0, 1]
-    for j in range(m):
-        a[j] = int(a[j])
-        for k in range(j + 1, m):
-            a[k] = int(a[k])
-            if a[j] ^ a[k] < a[min_[0]] ^ a[min_[1]]:
-                min_ = [j, k]
-    min_value.append(a[min_[0]] ^ a[min_[1]])
-for i in min_value:
-    print(i)
+    m_ = int(input()) # количество лежаков в тесте
+    m.append(m_)
+    spam = input().split() # массив из чисел, характеризующих лежаки
+    for j in range(m_):
+        spam[j] = int(spam[j])
+    a.append(spam)
+
+# Выполняем сравнения в тестах
+for i in range(n):
+    m_ = m.popleft() # количество лежаков в тесте
+    a_ = a.popleft() # список из чисел, характеризующих лежаки в тесте
+    min_ = a_[0] ^ a_[1] # минимальное значение непохожести двух лежаков в тесте
+    for j in range(m_):
+        for k in range(j + 1, m_):
+            if a_[j] ^ a_[k] < min_:
+                min_ = a_[j] ^ a_[k]
+    print(min_)
